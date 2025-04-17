48e6d7a537166234d2c3143fafdf0127d9d91399
witmess <matakalanew@gmail.com>
2025-04-17 16:39:18 +0200

    feat: Add core components and sections for Nike website
    - Implemented Button component for consistent button styling.
    - Created Navbar component for site navigation with links.
    - Defined constants for navigation links, products, services, and reviews.
    - Developed CustomerReviews, Footer, Hero, Navbar, PopularProducts, Services, SpecialOffers, Subscribe, and SuperQuality sections.
    - Integrated statistics display in the Hero section.
    - Exported all sections from index.js for easier imports.

--- nike/src/App.jsx
+++ nike/src/App.jsx
@@ -1,7 +1,34 @@
-export default function App() {
-  return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
-}
+import { CustomerReviews, Footer, Hero, Services, SpecialOffers, 
+  Subscribe, PopularProducts, SuperQuality } from "./sections";
+import Nav from './components/nav'
+const App = () => (
+  <main className="relative">
+    <Nav/>
+    <section className="xl:padding-1 wide:padding-r padding-b">
+      <Hero />
+    </section>
+    <section className="padding">
+      <PopularProducts />
+    </section>
+    <section className="padding">
+      <SuperQuality />
+    </section>
+    <section className="padding-x py-10">
+      <Services />
+    </section>
+    <section className="padding">
+      <SpecialOffers />
+    </section>
+    <section className="bg-pale-blue padding-x">
+      <CustomerReviews />
+    </section>
+    <section className="padding-x sm:py-32 py-1 w-full">
+      <Subscribe />
+    </section>
+    <section className="bg-black padding-t pb-8">
+      <Footer />
+    </section>
+  </main>
+);
+
+export default App;
---
+++ nike/src/sections/index.js
@@ -0,0 +1,21 @@
+import Navbar from "./navbar";
+import Hero from "./hero";
+import PopularProducts from "./PopularProducts";
+import Services from "./services";
+import Subscribe from "./subscribe";
+import SpecialOffers from "./SpecialOffers";
+import Footer from "./footer";
+import CustomerReviews from "./customerReviews";
+import SuperQuality from "./SuperQuality";
+
+export {
+  Hero,
+  Navbar,
+  PopularProducts,
+  SuperQuality,
+  Services,
+  SpecialOffers,
+  CustomerReviews,
+  Subscribe,
+  Footer
+};
