debf7b927310e8c6dfb450e220274c3f986fbdfd
MatakalaWit-Web <matakalanew@gmail.com>
2025-07-21 14:22:54 +0200

    feat: Implement Hero section with dynamic shoe image selection; add ShoeCard component

--- nike/src/sections/Hero.jsx
+++ nike/src/sections/Hero.jsx
@@ -1,12 +1,16 @@
 import { arrowRight } from "../assets/icons"
-import { bigShoe1 } from "../assets/images"
+import { bigShoe1} from "../assets/images"
 import Button from "../components/Button"
-import { statistics } from "../constants"
+import ShoeCard from "../components/ShoeCard"
+import { statistics, shoes } from "../constants"
+import {useState } from 'react';
 
 const hero = () => {
+  const [bigShoeImg, setBigShoeImg] = useState(bigShoe1);
+
   return (
     <section id="home" className="w-full flex xl:flex-row flex-col justify-center min-h-screen gap-10 max-container border-2 border-red-500">
-      <div className="relative xl:w-/5 flex flex-col justify-center items-start w-full max-xl:padding-x pt-28">
+      <div className="relative xl:w-2/5 flex flex-col justify-center items-start w-full max-xl:padding-x pt-28">
         <p className="text-xl font-montserrat text-coral-red">Our Summer Collecrion</p>
         <h1 className="mt-10 font-palanquin text-8xl max-sm:[72px] max-sm:leading-[82] font-bold">
           <span className="xl:bg-white xl:whitespace-nowrap relative z-10 pr-10">The New Arrival</span>
@@ -16,6 +20,7 @@ const hero = () => {
         <p className="font-montserrat text-slate-gray text-lg leading-8 mt-6 mb-14 sm:max-w-sm">Discover stylish Nike arrivals, quality comfort,
           and innovation for your active life.</p>
           <Button label="Shop now" iconURL={arrowRight}/>
+          
           <div className="flex justify-start items-start flex-wrap w-full mt-20 gap-16">
             {statistics.map((stat) => (
               <div key={stat.label}>
@@ -27,11 +32,22 @@ const hero = () => {
       </div>
 
       <div className="relative flex-1 flex justify-center items-center xl:min-h-screen max-xl:py-40 bg-primary bg-hero bg-cover bg-center">
-        <img src={bigShoe1}
+        <img src={bigShoeImg}
          alt="shoe collection" 
          width={610} height={500} 
          className="object-contain relative z-10"/>
       </div>
+
+      <div className="flex sm:gap-6 gap-4 absolute -bottom-[5%] sm:left-[10%] max-sm:px-6">
+        {shoes.map((shoe) => (
+          <div key={shoe}>
+            <ShoeCard imgURL={shoe} changeBigShoeImage={(shoe) => setBigShoeImg(shoe)} bigShoeImg={bigShoeImg} />
+
+          </div>
+        ))}
+      </div>
+
+
 
     </section>
   )
